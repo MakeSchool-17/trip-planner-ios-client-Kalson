3eadcecf162cb8a6953ad0a63154ad36868122e9
Kalson Kalu <Kalson.Kalu@yahoo.com>
2015-11-05 17:37:14 -0800

    update

--- TripPlanner/TripPlanner/AddTripController.swift
+++ TripPlanner/TripPlanner/AddTripController.swift
@@ -8,16 +8,48 @@
 
 import UIKit
 
-class AddTripController: UIViewController {
+class AddTripController: UIViewController, UITextFieldDelegate {
 
 
     @IBOutlet weak var cancelButton: UIBarButtonItem!
+    @IBOutlet weak var addButton: UIBarButtonItem!
+    
+    @IBOutlet weak var tripNameTextField: UITextField!
+    
+    var tripName: [[String:String]] = []
+    
+    @IBAction func addNewTrip(sender: AnyObject) {
+        addThoseTrips()
+    }
+    
     @IBAction func cancelButtonAction(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        addThoseTrips()
+        return true
+    }
+    
+    func addThoseTrips(){
+        if let text = tripNameTextField.text {
+            self.tripName.append(["name": text]) // optional binding, no longer an optional
+        }
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+        
+        print(tripName)
+        for name in tripName{
+            print("the trips = \(name)")
+        }
+    }
+    
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         
         
 
@@ -25,18 +57,10 @@ class AddTripController: UIViewController {
     }
     
     override func viewWillAppear(animated: Bool) {
+        
+        self.tripNameTextField.delegate = self // tell the tripNameTextField that its the delegate for the UItextField protocol
         self.cancelButton = UIBarButtonItem(title: "Yay", style: .Done, target: self, action: Selector("cancelButtonAction"))
     }
-    
-//    @objc func cancelButtonAction(){
-//        print("cancel button was pressed")
-//        self.dismissViewControllerAnimated(true, completion: nil)
-//    }
-//    
-//    func sayHello(sender: UIBarButtonItem) {
-//        print("Hello")
-//    }
-
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
---
+++ TripPlanner/TripPlanner/PlanTripsTVC.swift
@@ -0,0 +1,117 @@
+//
+//  PlanTripsTVC.swift
+//  TripPlanner
+//
+//  Created by KaL on 11/5/15.
+//  Copyright © 2015 KaL. All rights reserved.
+//
+
+import UIKit
+
+class PlanTripsTVC: UITableViewController {
+    
+    var addTripC = AddTripController()
+    var tripNames = [:]
+    var tripName: [[String:String]] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        print("Trips added = \(self.tripName)")
+        self.tableView.reloadData()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return tripName.count
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+
+        // Configure the cell...
+        cell.textLabel?.text = addTripC.tripName[indexPath.row]["name"]
+        
+        return cell
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        if segue.identifier == "addTrips"{
+            let navController = segue.destinationViewController as! UINavigationController
+            let destinationController = navController.topViewController as! AddTripController
+            destinationController.tripName = tripName
+            
+        }
+        
+    }
+
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
